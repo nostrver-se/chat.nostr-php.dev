f2f27d56346a0208a014a5415ba47462eaa65ad1
Rik Meijer <nostriphant@rikmeijer.nl>
2026-03-10 10:33:17 +0100

    refactor tests

--- tests/AcceptanceCase.php
+++ tests/AcceptanceCase.php
@@ -4,61 +4,61 @@ namespace nostriphant\TranspherTests;
 
 use nostriphant\NIP01\Key;
 use PHPUnit\Framework\TestCase as BaseTestCase;
-use nostriphant\TranspherTests\Feature\Process;
 use nostriphant\NIP19\Bech32;
 
 
 abstract class AcceptanceCase extends BaseTestCase
 {   
     static function start_transpher(string $port, \nostriphant\NIP01\Key $owner, ?array $whitelisted_npubs) {
-        $data_dir = ROOT_DIR . '/data/relay_' . $port;
-        is_dir($data_dir) || mkdir($data_dir);
-        
-    
-        (is_file($data_dir . '/transpher.sqlite') === false) ||  unlink($data_dir . '/transpher.sqlite');
-        expect($data_dir . '/transpher.sqlite')->not()->toBeFile();
-        
-        $relay_env = [
-            'AGENT_NSEC' => (string) 'nsec1ffqhqzhulzesndu4npay9rn85kvwyfn8qaww9vsz689pyf5sfz7smpc6mn',
-            'RELAY_URL' => 'ws://127.0.0.1:' . $port,
-            'RELAY_OWNER_NPUB' => (string) Bech32::npub($owner(Key::public())),
-            'RELAY_NAME' => 'Really relay',
-            'RELAY_DESCRIPTION' => 'This is my dev relay',
-            'RELAY_CONTACT' => 'transpher@nostriphant.dev',
-            'RELAY_DATA' => $data_dir,
-            'RELAY_LOG_LEVEL' => 'DEBUG',
-            'LIMIT_EVENT_CREATED_AT_LOWER_DELTA' => 60 * 60 * 72, // to accept NIP17 pdm created_at randomness
-            'BLOSSOM_SERVER_KEY' => 'ae89403ee4f95cac13c9984f588ad92cee48c202f52c6f96d4d5c053d8332c85',
-        ];
-        if (isset($whitelisted_npubs)) {
-            $relay_env['RELAY_WHITELISTED_AUTHORS_ONLY'] = 1;
-            $relay_env['RELAY_WHITELISTED_AUTHORS'] = implode(',', $whitelisted_npubs);
+        return new class($port, $owner, $whitelisted_npubs) {
+            private Relay $relay;
+            private Agent $agent;
+            public string $data_directory;
+            public string $url;
+            public string $ws;
             
-        }
+            public function __construct(string $port, \nostriphant\NIP01\Key $owner, ?array $whitelisted_npubs) {
+                $this->data_directory = ROOT_DIR . '/data/relay_' . $port;
+                is_dir($this->data_directory) || mkdir($this->data_directory);
 
-        $relay = new Relay('tcp://127.0.0.1:' . $port, $relay_env);
-        
-        $agent = new Agent($port, [
-            'RELAY_OWNER_NPUB' => (string) Bech32::npub($owner(Key::public())),
-            'AGENT_NSEC' => (string) 'nsec1ffqhqzhulzesndu4npay9rn85kvwyfn8qaww9vsz689pyf5sfz7smpc6mn',
-            'RELAY_URL' => $relay_env['RELAY_URL'],
-            'AGENT_LOG_LEVEL' => 'DEBUG',
-        ]);
-        
-        sleep(4);
-        
-        $transpher = fn() => $relay() || $agent();
-        
-        
-        return new class($relay_env['RELAY_DATA'], 'http://127.0.0.1:' . $port, $relay_env['RELAY_URL'], $transpher) {
-            private \Closure $transpher;
-            
-            public function __construct(public string $data_directory, public string $url, public string $ws, callable $transpher) {
-                $this->transpher = \Closure::fromCallable($transpher);
+                $this->url = 'http://127.0.0.1:' . $port;
+                $this->ws = 'ws://127.0.0.1:' . $port;
+
+                (is_file($this->data_directory . '/transpher.sqlite') === false) ||  unlink($this->data_directory . '/transpher.sqlite');
+                expect($this->data_directory . '/transpher.sqlite')->not()->toBeFile();
+
+                $relay_env = [
+                    'AGENT_NSEC' => (string) 'nsec1ffqhqzhulzesndu4npay9rn85kvwyfn8qaww9vsz689pyf5sfz7smpc6mn',
+                    'RELAY_URL' => $this->ws,
+                    'RELAY_OWNER_NPUB' => (string) Bech32::npub($owner(Key::public())),
+                    'RELAY_NAME' => 'Really relay',
+                    'RELAY_DESCRIPTION' => 'This is my dev relay',
+                    'RELAY_CONTACT' => 'transpher@nostriphant.dev',
+                    'RELAY_DATA' => $this->data_directory,
+                    'RELAY_LOG_LEVEL' => 'DEBUG',
+                    'LIMIT_EVENT_CREATED_AT_LOWER_DELTA' => 60 * 60 * 72, // to accept NIP17 pdm created_at randomness
+                    'BLOSSOM_SERVER_KEY' => 'ae89403ee4f95cac13c9984f588ad92cee48c202f52c6f96d4d5c053d8332c85',
+                ];
+                if (isset($whitelisted_npubs)) {
+                    $relay_env['RELAY_WHITELISTED_AUTHORS_ONLY'] = 1;
+                    $relay_env['RELAY_WHITELISTED_AUTHORS'] = implode(',', $whitelisted_npubs);
+
+                }
+
+                $this->relay = new Relay('tcp://127.0.0.1:' . $port, $relay_env);
+
+                $this->agent = new Agent($port, [
+                    'RELAY_OWNER_NPUB' => (string) Bech32::npub($owner(Key::public())),
+                    'AGENT_NSEC' => (string) 'nsec1ffqhqzhulzesndu4npay9rn85kvwyfn8qaww9vsz689pyf5sfz7smpc6mn',
+                    'RELAY_URL' => $relay_env['RELAY_URL'],
+                    'AGENT_LOG_LEVEL' => 'DEBUG',
+                ]);
+                sleep(4);
             }
             
             public function __invoke() {
-                call_user_func($this->transpher);
+                call_user_func($this->relay);
+                call_user_func($this->agent);
             }
         };
     }
